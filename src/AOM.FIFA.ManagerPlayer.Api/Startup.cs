4ab561670a79561521448c59222a47f62e9a2fbd
Adhan Maldonado <adhan.maldonado@anova.com>
2022-06-06 19:42:36 +0100

    removing old references

--- src/AOM.FIFA.ManagerPlayer.Api/Startup.cs
+++ src/AOM.FIFA.ManagerPlayer.Api/Startup.cs
@@ -1,9 +1,8 @@
+using AOM.FIFA.ManagerPlayer.Api.Extensions.Build;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using AOM.FIFA.ManagerPlayer.Api.Extensions.Build;
-using Hangfire;
 
 namespace AOM.FIFA.ManagerPlayer.Api
 {
@@ -14,8 +13,7 @@ namespace AOM.FIFA.ManagerPlayer.Api
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services) => services.Build(Configuration);
-
-        //IBackgroundJobClient backgroundJobs
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) => app.Build(env);
         
     }
